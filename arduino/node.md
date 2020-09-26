6de6db305f06f5c52f2a2f01e83fd26a6cd6c094
Ray Zhang <melodypapa@outlook.com>
2020-09-26 15:42:33 +0800

    add the example for the arduino node

---
+++ arduino/node.md
@@ -0,0 +1,17 @@
+# Arduino & Node.js
+
+## Install the necessary packages
+
+```
+npm install johnny-five @types/johnny-five --save-dev
+```
+
+Note: Try to execute with the typescript, the ts-node module can be installed
+
+```
+npm install ts-node --save-dev
+```
+
+## Example 
+
+* [blink](/examples/arduino/arduino_node/blink.ts)
---
+++ examples/arduino/arduino_node/blink.js
@@ -0,0 +1,9 @@
+var five =require('johnny-five'); //require/import the johnny-five module
+var board=new five.Board(); //the Board class from johnny-five module
+
+//When the board is ready create a led object on pin 13 and blink it 
+//every 500 milliseconds.
+
+board.on('ready',function(){
+    var led=new five.Led(13);
+    led.blink(500);}); 
